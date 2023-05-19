7c81af4cfe639b68dbd1e21d9b2cf270a1231e87
Andrei Nicholson <contact@andreinicholson.com>
2023-05-18 22:41:29 -0400

    Migrate to .NET 7 (#2)

--- MVMP3/Program.cs
+++ MVMP3/Program.cs
@@ -4,37 +4,38 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 
-namespace MVMP3
+namespace MVMP3;
+
+[Command(Description = "")]
+internal class Program
 {
-    [Command(Description = "")]
-    internal class Program
+    [Required]
+    [Option("--source", "Base directory for MP3 files to search under.", CommandOptionType.SingleValue)]
+    public string SourcePath { get; }
+
+    [Option]
+    public bool Verbose { get; }
+
+    private static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+    public void OnExecute()
     {
-        [Required]
-        [Option("--source", "Base directory for MP3 files to search under.", CommandOptionType.SingleValue)]
-        public string SourcePath { get; }
+        var destinationPath = Directory.GetCurrentDirectory();
 
-        [Option]
-        public bool Verbose { get; }
+        Console.WriteLine($"Source: {SourcePath}");
+        Console.WriteLine($"Searching '{SourcePath}' for music...");
 
-        private static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+        var musicMapper = new MusicMapper(SourcePath);
+        musicMapper.Map();
 
-        public void OnExecute()
+        musicMapper.DisplaySummary();
+
+        Console.WriteLine($"Saving files under '{destinationPath}'");
+
+        var fileMapper = new FileMapper(destinationPath, musicMapper.Artists)
         {
-            var destinationPath = Directory.GetCurrentDirectory();
-
-            Console.WriteLine($"Source: {SourcePath}");
-            Console.WriteLine($"Searching '{SourcePath}' for music...");
-
-            var musicMapper = new MusicMapper(SourcePath);
-            musicMapper.Map();
-
-            musicMapper.DisplaySummary();
-
-            Console.WriteLine($"Saving files under '{destinationPath}'");
-
-            var fileMapper = new FileMapper(destinationPath, musicMapper.Artists);
-            fileMapper.Verbose = Verbose;
-            fileMapper.Map();
-        }
+            Verbose = Verbose
+        };
+        fileMapper.Map();
     }
 }
